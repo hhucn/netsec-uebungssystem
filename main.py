1c043ec886558b1f0e89533dfda0ebeca6026177
Martin <dfmogk@gmail.com>
2014-10-21 15:42:45 +0200

    Calling rule functions dynamically, dropping many lines of code

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ import logging
 def main():
 	# patching imaplib
 	imaplib.Commands["MOVE"]=("SELECTED",)
-	imaplib.Commands["IDLE"]=("SELECTED",)
 
 	# Parsing config.json, making the settings global
 	global smtpmail_server,imapmail_server,mail_address,mail_password,loglevel,logmethod,delay
@@ -46,8 +45,13 @@ def main():
 	delay = settings.get("delay")
 
 	imapmail = login()
+	imapmail.select("INBOX")
+
+	for rule in rules:
+		processRule(imapmail,rule["steps"])
+
 	imapmail.send("%s IDLE\r\n"%imapmail._new_tag())
-	
+
 	if "idling" in imapmail.readline():
 		logging.debug("Server supports IDLE.")
 		while(True):
@@ -56,13 +60,13 @@ def main():
 				imapmail.readline()
 
 				for rule in rules:
-					processRule(imapmail,rule)
+					processRule(imapmail,rule["steps"])
 				imapmail.send("%s IDLE\r\n"%imapmail._new_tag())
 	else:
 		logging.debug("Server lacks support for IDLE... Falling back to delay.")
 		while(True):
 			for rule in rules:
-				processRule(imapmail,rule)
+				processRule(imapmail,rule["steps"])
 			time.sleep(delay)
 
 def login():
@@ -74,58 +78,62 @@ def login():
 	return imapmail
 
 def processRule(imapmail,rule):
-	for step in rule["steps"]:
-		action = step[0]
-		steplen = len(step)
-		if action == "filter":
-			if steplen==3:
-				id_list = filterMailbox(imapmail,step[1],step[2])
-			elif steplen==4:
-				id_list = filterMailbox(imapmail,step[1],step[2],step[3])
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		elif action == "answer":
-			if steplen==4:
-				id_list = answerMails(imapmail,id_list,step[1],step[2],step[3])
-			elif steplen==3:
-				id_list = answerMails(imapmail,id_list,step[1],step[2],"")
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		elif action == "move":
-			if steplen==2:
-				id_list = moveMails(imapmail,id_list,step[1])
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		elif action == "log":
-			if steplen==3:
-				if step[1] == "ERROR":
-					logging.error(step[2])
-				elif step[1] == "CRITICAL":
-					logging.critical(step[2])
-				else:
-					logging.info(step[2])
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		elif action == "flag":
-			if steplen==2:
-				id_list = flagMails(imapmail,id_list,step[1])
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		elif action == "delete":
-			if steplen==1:
-				flagMails(imapmail,id_list,"\\Deleted")
-				imapmail.expunge()
-			else:
-				logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
-				sys.exit(2)
-		else:
-			logging.critical("Rule <%s>, Step <%s>: step is unknown."%(rule["title"],action))
-			sys.exit(2)
+	id_list = []
+	for step in rule:
+		#print "imapmail,",id_list,step[1:]
+		id_list = globals()["rule_" + step[0]](imapmail,id_list,*step[1:])
+	# for step in rule["steps"]:
+	# 	action = step[0]
+	# 	steplen = len(step)
+	# 	if action == "filter":
+	# 		if steplen==3:
+	# 			id_list = filterMailbox(imapmail,step[1],step[2])
+	# 		elif steplen==4:
+	# 			id_list = filterMailbox(imapmail,step[1],step[2],step[3])
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	elif action == "answer":
+	# 		if steplen==4:
+	# 			id_list = answerMails(imapmail,id_list,step[1],step[2],step[3])
+	# 		elif steplen==3:
+	# 			id_list = answerMails(imapmail,id_list,step[1],step[2],"")
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	elif action == "move":
+	# 		if steplen==2:
+	# 			id_list = moveMails(imapmail,id_list,step[1])
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	elif action == "log":
+	# 		if steplen==3:
+	# 			if step[1] == "ERROR":
+	# 				logging.error(step[2])
+	# 			elif step[1] == "CRITICAL":
+	# 				logging.critical(step[2])
+	# 			else:
+	# 				logging.info(step[2])
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	elif action == "flag":
+	# 		if steplen==2:
+	# 			id_list = rule_flag(imapmail,id_list,step[1])
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	elif action == "delete":
+	# 		if steplen==1:
+	# 			rule_flag(imapmail,id_list,"\\Deleted")
+	# 			imapmail.expunge()
+	# 		else:
+	# 			logging.critical("Rule <%s>, Step <%s>: wrong argument count."%(rule["title"],action))
+	# 			sys.exit(2)
+	# 	else:
+	# 		logging.critical("Rule <%s>, Step <%s>: step is unknown."%(rule["title"],action))
+	# 		sys.exit(2)
 
 
 
@@ -133,7 +141,7 @@ def processRule(imapmail,rule):
 # procession functions
 #
 
-def filterMailbox(imapmail,filterVariable,filterValue,mailbox="Inbox"):
+def rule_filter(imapmail,id_list,filterVariable,filterValue,mailbox="INBOX"):
 	# returns all mails where filterVariable == filterValue
 	# see http://tools.ietf.org/html/rfc3501#section-6.4.4 (for search)
 	# and http://tools.ietf.org/html/rfc3501#section-6.4.5 (for fetch)
@@ -158,7 +166,7 @@ def filterMailbox(imapmail,filterVariable,filterValue,mailbox="Inbox"):
 					uidlist.append(uid)
 	return uidlist
 
-def answerMails(imapmail,id_list,subject,text,address):
+def rule_answer(imapmail,id_list,subject,text,address="(back)"):
 	# see http://tools.ietf.org/html/rfc3501#section-6.4.6 (for store)
 	for uid in id_list:
 		hashobj = hashlib.md5()
@@ -167,11 +175,11 @@ def answerMails(imapmail,id_list,subject,text,address):
 
 		result, data = imapmail.uid("fetch", uid, "(BODY[HEADER.FIELDS (FROM)])")
 		rawMail = data[0][1]
-		if(address):
-			client_mail_addr = address
-		else:
+		if address == "(back)":
 			client_mail_addr = rawMail[rawMail.find("<")+1:rawMail.find(">")]
-
+		else:
+			client_mail_addr = address
+		
 		if "NETSEC-Answered-" + subject_hash in imapmail.uid("fetch",uid,"FLAGS")[1][0]:
 			logging.error("Error: Tried to answer to mail (uid %s, addr '%s', Subject '%s') which was already answered."%(uid,client_mail_addr,subject))
 		else:
@@ -181,11 +189,11 @@ def answerMails(imapmail,id_list,subject,text,address):
 				logging.error("Error: Tried to answer automated mail. (uid %i, addr '%s' Subject '%s')"%(uid,client_mail_addr,subject))
 			else:
 				smtpMail(client_mail_addr,"Subject: %s\n\n%s"%(subject,text))
-				flagMails(imapmail,uid,"NETSEC-Answered-" + subject_hash)
+				###rule_flag(imapmail,uid,"NETSEC-Answered-" + subject_hash)
 				#imapmail.uid("STORE",uid,"+FLAGS","NETSEC-Answered-" + subject_hash)
 	return id_list
 
-def moveMails(imapmail,id_list,destination):
+def rule_move(imapmail,id_list,destination):
 	# moves the mails from id_list to mailbox destination
 	# warning: this alters the UID of the mails!
 	imapmail.create(destination)
@@ -195,7 +203,7 @@ def moveMails(imapmail,id_list,destination):
 		if result != "OK":
 			logging.error("Error moving uid%s to %s"%(uid,destination))
 
-def flagMails(imapmail,id_list,flag):
+def rule_flag(imapmail,id_list,flag):
 	for uid in id_list:
 		imapmail.uid("STORE",uid,"+FLAGS",flag)
 	return id_list

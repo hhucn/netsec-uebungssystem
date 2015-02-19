f66ec06ec03c488e331a3ad5e3ca0feebde940c7
Philipp Hagemeister <phihag@phihag.de>
2015-02-19 17:57:43 +0100

    Add more source code checks (#3)

--- test/test_source_quality.py
+++ test/test_source_quality.py
@@ -43,6 +43,35 @@ class TestSourceQuality(unittest.TestCase):
                 'in %s, around %s' % (
                     fn, code[m.start() - 10:m.end() + 10]))
 
+    def _test_strange_is(self, fn, code):
+        ''' is tests whether two names refer to the same object.
+        This is useful when the object is guaranteed to exist only once,
+        as is the case with None.
+
+        There is no such guarantee for integers (the behavior depends on the
+        implementation). See http://stackoverflow.com/q/306313/35070 for
+        more details.
+        '''
+        m = re.search(
+            r'(?m)^\s*(.*?\s+is\s+[0-9]+.*?)\s*$', code)
+        if m:
+            self.assertFalse(
+                m,
+                'Very strange is comparison in %s, in line %r' % (
+                    fn, m.group(1)))
+
+    def _test_unnecessary_parens(self, fn, code):
+        ''' Instead of while(True): , which looks like C code, in Python we can
+        dispose of the parentheses and simply write while True:'''
+        m = re.search(
+            r'(?m)^\s*(if|while)\((.{,20})\):\s*$', code)
+        if m:
+            self.assertFalse(
+                m,
+                'Are the parentheses in line %r in file %s really necessary? '
+                'How about  %s %s:  ?' % (
+                    m.group(0), fn, m.group(1), m.group(2)))
+
     def test_all_files(self):
         for dirpath, _, filenames in os.walk(rootDir):
             for basename in filenames:
@@ -55,6 +84,8 @@ class TestSourceQuality(unittest.TestCase):
 
                 self._test_unicode_literals(fn, code)
                 self._test_regexp_rawstrings(fn, code)
+                self._test_strange_is(fn, code)
+                self._test_unnecessary_parens(fn, code)
 
 if __name__ == '__main__':
     unittest.main()

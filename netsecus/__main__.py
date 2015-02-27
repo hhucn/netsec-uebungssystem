ca317034a6c582838190165165d3f4e882c9baa5
Philipp Hagemeister <phihag@phihag.de>
2015-02-27 10:25:25 +0100

    Allow direct execution

--- netsecus/__main__.py
+++ netsecus/__main__.py
@@ -1,6 +1,12 @@
 from __future__ import unicode_literals
 
-import __init__ as netsecus
+import netsecus
+
+if __package__ is None:
+    # direct call of __main__.py
+    import os.path
+    path = os.path.realpath(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(os.path.dirname(path)))
 
 if __name__ == '__main__':
     netsecus.main()

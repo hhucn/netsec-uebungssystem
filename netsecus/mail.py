cafe2fcf2f8dfc7608448934bdff220e9dd84686
maride <martin.dessauer@t-online.de>
2015-05-08 15:59:38 +0200

    Add mail alias resolver, merge databases

--- netsecus/mail.py
+++ netsecus/mail.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 class Mail(object):
 
-    def __init__(self, uid, var, text):
+    def __init__(self, uid, var, address, text):
         self.uid = uid
         self.variables = var
         self.text = text
+        self.address = address
         self.variables["MAILFROM"] = text["From"]
         self.variables["MAILDATE"] = text["Date"]
         self.variables["MAILRECEIVED"] = text["Received"]
---
+++ scripts/resolveMailAlias.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+import sqlite3
+import re
+
+from netsecus import helper
+from netsecus.mail import Mail
+
+
+def run(config, imapmail, mails, *args):
+    aliasDatabasePath = config("database_path")
+    aliasDatabase = sqlite3.connect(aliasDatabasePath) 
+    cursor = aliasDatabase.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS aliases (`alias` text UNIQUE, `destination` text UNIQUE, PRIMARY KEY (`alias`));")
+
+    for mail in mails:
+        mail.address["identifier"] = getDestinationForAlias(mail.address["identifier"], cursor)
+
+    return mails
+
+def getDestinationForAlias(alias, cursor):
+    cursor.execute("SELECT destination FROM aliases WHERE alias = ?", (alias,))
+    destination = cursor.fetchone()
+    if not destination:
+        return alias
+    else:
+        return getDestinationForAlias(destination[0], cursor)

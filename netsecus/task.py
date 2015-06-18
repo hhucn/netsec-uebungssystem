a06ca6899062bab407cb4c6884d0bf148a380b1f
maride <martin.dessauer@hhu.de>
2015-06-18 19:37:49 +0200

    Add a variable to hold reached points

--- netsecus/task.py
+++ netsecus/task.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 class Task(object):
 
-    def __init__(self, number, description, maxPoints):
+    def __init__(self, number, description, maxPoints, reachedPoints):
         self.number = number
         self.description = description
         self.maxPoints = maxPoints
+        self.reachedPoints = reachedPoints

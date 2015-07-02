cf865a30a193df41dc123f652a402df1b4136964
maride <martin.dessauer@hhu.de>
2015-07-02 10:59:26 +0200

    Add start and end time to sheet, replace name with UID as identifier for tasks and sheets

--- netsecus/sheet.py
+++ netsecus/sheet.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 class Sheet(object):
 
-    def __init__(self, number, tasks, editable, sheetID):
-        self.number = number
+    def __init__(self, sheetID, name, tasks, editable, timeStart=0, timeEnd=0):
+        self.id = sheetID
+        self.name = name
         self.tasks = tasks
         self.editable = editable
-        self.id = sheetID
+        self.timeStart = timeStart
+        self.timeEnd = timeEnd

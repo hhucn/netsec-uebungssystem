7c47a2960d644b34ce3ff569042fb5e965270e8c
maride <martin.dessauer@hhu.de>
2016-01-16 14:13:56 +0100

    Remove unneeded 'reachedPoints' variable from Task class

--- netsecus/task.py
+++ netsecus/task.py
@@ -3,10 +3,9 @@ from __future__ import unicode_literals
 
 class Task(object):
 
-    def __init__(self, taskID, sheetID, name, description, maxPoints, reachedPoints=0):
+    def __init__(self, taskID, sheetID, name, description, maxPoints):
         self.id = taskID
         self.sheetID = sheetID
         self.name = name
         self.description = description
         self.maxPoints = maxPoints
-        self.reachedPoints = reachedPoints

34ea78c76ae1ebabc0376542e77ac3f1f5f1f587
maride <martin.dessauer@hhu.de>
2016-03-10 11:19:06 +0100

    Remove protection for parallel edit

--- netsecus/webhandler/PointsHandler.py
+++ netsecus/webhandler/PointsHandler.py
@@ -9,26 +9,19 @@ class PointsHandler(NetsecHandler):
         identifier = self.get_argument("identifier")
         sheetNumber = self.get_argument("sheetNumber")
         taskNumber = self.get_argument("taskNumber")
-        oldPoints = self.get_argument("oldPoints")
         newPoints = self.get_argument("newPoints")
         database = Database(self.application.config)
         reachedPoints = database.getReachedPoints(sheetNumber, taskNumber, identifier)
-        maxPoints = 0
+        points = 0
 
         task = database.getTaskFromSheet(sheetNumber, taskNumber)
         if task:
-            maxPoints = task.maxPoints
+            points = task.points
 
-        if not float(oldPoints) == reachedPoints:
-            # Someone submitted new points after this user opened the detail page, but before he could submit his points
-            self.render("points", {"redirect": 0, "error": "modified", "oldPoints": oldPoints,
-                                   "reachedPoints": reachedPoints, "taskNumber": taskNumber, "sheetNumber": sheetNumber,
-                                   "identifier": identifier})
-        elif maxPoints < float(newPoints):
-            self.render("points", {"redirect": 0, "error": "overMaxPoints", "oldPoints": oldPoints,
-                                   "reachedPoints": reachedPoints, "taskNumber": taskNumber, "sheetNumber": sheetNumber,
-                                   "identifier": identifier})
+        if points < float(newPoints):
+            self.render("points", {"redirect": 0, "error": "overMaxPoints", "reachedPoints": reachedPoints,
+                                   "taskNumber": taskNumber, "sheetNumber": sheetNumber, "identifier": identifier})
         else:
             database.setReachedPoints(sheetNumber, taskNumber, identifier, newPoints)
-            self.render("points", {"redirect": 1, "error": "", "oldPoints": oldPoints, "reachedPoints": newPoints,
-                        "taskNumber": taskNumber, "sheetNumber": sheetNumber, "identifier": identifier})
+            self.render("points", {"redirect": 1, "error": "", "reachedPoints": newPoints, "taskNumber": taskNumber,
+                        "sheetNumber": sheetNumber, "identifier": identifier})

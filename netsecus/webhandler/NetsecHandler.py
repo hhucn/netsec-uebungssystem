e4d95f090f76aa5fc268a039c06fb706f2b0d41f
Philipp Hagemeister <phihag@phihag.de>
2016-03-17 15:25:53 +0100

    rename htmldocs to templates to clarify it is not about documentation

--- netsecus/webhandler/NetsecHandler.py
+++ netsecus/webhandler/NetsecHandler.py
@@ -7,7 +7,7 @@ from .. import helper
 
 class NetsecHandler(helper.RequestHandlerWithAuth):
     def render(self, template, data):
-        TEMPLATE_PATH = os.path.join(self.application.config.module_path, "htmldocs")
+        TEMPLATE_PATH = os.path.join(self.application.config.module_path, "templates")
 
         super(NetsecHandler, self).render(
             os.path.join(TEMPLATE_PATH, "%s.html" % template),

36d96da1552005052a911b54669a12bcc0f7245a
maride <martin.dessauer@hhu.de>
2016-03-31 17:00:52 +0200

    Redirect as TableHandler has currently no purpose

--- netsecus/webhandler/TableHandler.py
+++ netsecus/webhandler/TableHandler.py
@@ -6,18 +6,4 @@ from .NetsecHandler import NetsecHandler
 
 class TableHandler(NetsecHandler):
     def get(self):
-        database = Database(self.application.config)
-        sheets = database.getSheets()
-
-        # Count submissions for a sheet
-        for sheet in sheets:
-            sheetSubmissions = database.getSubmissionForSheet(sheet.id)
-            sheet.submissions = len(sheetSubmissions)
-
-            sheetSubmissionsFinished = 0
-            for submission in sheetSubmissions:
-                if submission.finished:
-                    sheetSubmissionsFinished = sheetSubmissionsFinished + 1
-            sheet.submissionsFinished = sheetSubmissionsFinished
-
-        self.render("table", {"sheets": sheets})
+        self.redirect("/sheets")

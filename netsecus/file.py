a433d9b6f3b6714faf18f7051e0e5ea0e83a9b62
maride <martin.dessauer@hhu.de>
2016-04-07 19:11:32 +0200

    Add files to submission overview page

--- netsecus/file.py
+++ netsecus/file.py
@@ -6,7 +6,7 @@ File = collections.namedtuple("File", ["id", "submission_id", "hash", "filename"
 
 def get_for_submission(db, submission_id):
     db.cursor.execute("SELECT id, submission_id, hash, filename FROM file WHERE submission_id = ?", (submission_id, ))
-    rows = self.cursor.fetchall()
+    rows = db.cursor.fetchall()
     result = []
 
     for row in rows:
--- netsecus/webhandler/SubmissionDetailHandler.py
+++ netsecus/webhandler/SubmissionDetailHandler.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 from .NetsecHandler import NetsecHandler
 
 from .. import submission
+from .. import file
 
 class SubmissionDetailHandler(NetsecHandler):
     def get(self, submission_id):
         requested_submission = submission.get_from_id(self.application.db, submission_id)
-        self.render('submissionDetail', {'submission': requested_submission})
+        submission_files = file.get_for_submission(self.application.db, requested_submission.id)
+        self.render('submissionDetail', {'submission': requested_submission, 'files': submission_files})

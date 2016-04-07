67791bbc0c5dd4d242d6a866effb40a5e9ed96cf
maride <martin.dessauer@hhu.de>
2016-04-07 18:11:19 +0200

    Update "/sheets" to new database format

--- netsecus/webhandler/SheetsHandler.py
+++ netsecus/webhandler/SheetsHandler.py
@@ -2,10 +2,18 @@ from __future__ import unicode_literals
 
 from ..database import Database
 from .NetsecHandler import NetsecHandler
+from .. import task
 
 
 class SheetsHandler(NetsecHandler):
     def get(self):
         database = Database(self.application.config)
         sheets = database.getSheets()
-        self.render('sheets', {'sheets': sheets})
+        sheets_tasks = []
+
+        for sheet in sheets:
+            tasks_for_sheet = task.get_for_sheet(database, sheet.id)
+            if tasks_for_sheet:
+                sheets_tasks.append(tasks_for_sheet)
+
+        self.render('sheets', {'sheets_tasks': sheets_tasks, 'sheets': sheets})

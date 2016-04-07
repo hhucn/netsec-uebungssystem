16ebfc859cf3a5a07b9be41da722cb4724251867
maride <martin.dessauer@hhu.de>
2016-04-07 17:53:38 +0200

    Move sheet-specific database operations to sheet.py

--- netsecus/sheet.py
+++ netsecus/sheet.py
@@ -14,6 +14,30 @@ def get_by_id(database, sheet_id):
     else:
         return None
 
+def get_all(database):
+    database.cursor.execute("SELECT id, end, deleted FROM sheet")
+    rows = database.cursor.fetchall()
+
+    for row in rows:
+        result.append(Sheet(*row))
+
+    return result
+
 def create(database):
     database.cursor.execute("INSERT INTO sheet (end, deleted) VALUES (NULL, 0)")
     database.commit()
+
+def delete(database, sheet_id):
+    database.cursor.execute("""UPDATE sheet SET deleted = 1 WHERE id = ?""",
+                               (sheet_id, ))
+    database.database.commit()
+
+def restore_sheet(database, sheet_id):
+    database.cursor.execute("""UPDATE sheet SET deleted = 0 WHERE id = ?""",
+                               (sheet_id, ))
+    database.database.commit()
+
+def edit_end(database, sheet_id, end):
+    database.cursor.execute("""UPDATE sheet SET end=? WHERE id = ?""",
+                               (end, sheet_id))
+    database.database.commit()

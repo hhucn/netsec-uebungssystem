ecb588f14c698f409e1406deececfb89ff7f7b22
maride <martin.dessauer@hhu.de>
2016-04-15 19:32:09 +0200

    Serve submitted files

--- netsecus/korrekturserver.py
+++ netsecus/korrekturserver.py
@@ -21,6 +21,7 @@ from .webhandler.StudentHandler import StudentHandler
 from .webhandler.SubmissionsHandler import SubmissionsHandler
 from .webhandler.SubmissionDetailHandler import SubmissionDetailHandler
 from .webhandler.SubmissionGradeHandler import SubmissionGradeHandler
+from .webhandler.DownloadHandler import DownloadHandler
 from .webhandler.SubmissionStudentSheetHandler import SubmissionStudentSheetHandler
 
 from . import database
@@ -66,6 +67,7 @@ def mainloop(config):
         (r"/submission/([0-9]+)", SubmissionDetailHandler),
         (r"/submission/([0-9]+)/([0-9]+)", SubmissionStudentSheetHandler),
         (r"/submission/([0-9]+)/grade", SubmissionGradeHandler),
+        (r"/download/(.*)", DownloadHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {
             "path": os.path.join(config.module_path, "static")
         }),

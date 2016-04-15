30282aa98a4cf73df30ba511fa663f10a063e5ca
Philipp Hagemeister <phihag@phihag.de>
2016-04-15 16:04:28 +0200

    show all sheets

--- netsecus/webhandler/SheetsHandler.py
+++ netsecus/webhandler/SheetsHandler.py
@@ -8,11 +8,9 @@ from .. import sheet
 class SheetsHandler(NetsecHandler):
     def get(self):
         sheets = sheet.get_all(self.application.db)
-        sheets_tasks = []
 
-        for single_sheet in sheets:
-            tasks_for_sheet = task.get_for_sheet(self.application.db, single_sheet.id)
-            if tasks_for_sheet:
-                sheets_tasks.append(tasks_for_sheet)
+        sheets_tasks = (
+            task.get_for_sheet(self.application.db, single_sheet.id)
+        for single_sheet in sheets)
 
         self.render('sheets', {'sheets_tasks': sheets_tasks, 'sheets': sheets})

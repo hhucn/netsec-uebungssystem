346160dac2769c51d474b92d9e25344895beedee
maride <martin.dessauer@hhu.de>
2016-04-19 12:57:26 +0200

    Try to guess mimetypes

--- netsecus/webhandler/DownloadHandler.py
+++ netsecus/webhandler/DownloadHandler.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import os.path
+import mimetypes
 
 from ..helper import RequestHandlerWithAuth
 
@@ -15,8 +16,14 @@ class DownloadHandler(RequestHandlerWithAuth):
         requested_file_relpath = os.path.join(file_submission.files_path, requested_file.filename)
         requested_file_abspath = os.path.join(self.application.config.module_path, requested_file_relpath)
 
-        self.set_header("Content-Type", "application/octet-stream")
+        requested_file_type, encoding = mimetypes.guess_type(requested_file.filename)
+
+        if not requested_file_type:
+            requested_file_type = "application/octet-stream"
+
+        self.set_header("Content-Type", requested_file_type)
         self.set_header("Content-Disposition", "attachment; filename=%s" % requested_file.filename)
+
         with open(requested_file_abspath, "rb") as requested_file_handle:
             content = requested_file_handle.read()
             self.write(content)

deca3be1ee16f2212c7a875543f52e6849c4ee66
maride <martin.dessauer@hhu.de>
2016-04-19 13:39:17 +0200

    Show last grader and status in submission overview

--- netsecus/grading.py
+++ netsecus/grading.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 
 import collections
 
+from . import task
+
 Grading = collections.namedtuple('Grading', ['id', 'submission_id', 'task_id',
                                  'comment', 'time', 'decipoints', 'grader'])
 
@@ -30,3 +32,29 @@ def set_grade_for_task(db, task_id, submission_id, comment, time, decipoints, gr
         db.cursor.execute("""INSERT INTO grading (task_id, submission_id, comment, time, decipoints, grader)
                              VALUES(?, ?, ?, ?, ?, ?)""", (task_id, submission_id, comment, time, decipoints, grader))
     db.database.commit()
+
+def get_all_graders(db, submission_id):
+    db.cursor.execute("SELECT grader FROM grading WHERE submission_id = ?", (submission_id, ))
+    rows = db.cursor.fetchall()
+    all_graders = []
+
+    for row in rows:
+        grader = row[0]
+        if grader not in all_graders:
+            all_graders.append(grader)
+
+    return all_graders
+
+def get_submission_grade_status(db, submission_id):
+    db.cursor.execute("SELECT decipoints FROM grading WHERE submission_id = ?", (submission_id, ))
+    graded_amount = len(db.cursor.fetchall())
+
+    if graded_amount == 0:
+        return "Unbearbeitet"
+
+    task_amount = len(task.get_for_sheet(db, submission_id))
+
+    if task_amount > graded_amount:
+        return "Angefangen"
+
+    return "Fertig"

c775f93cf82c065a591cb6d495ede882ca8f8e8e
maride <martin.dessauer@hhu.de>
2016-04-19 13:57:44 +0200

    Switch editing on SubmissionsList{All,Current)Handler

--- netsecus/webhandler/SubmissionsListAllHandler.py
+++ netsecus/webhandler/SubmissionsListAllHandler.py
@@ -9,9 +9,4 @@ from .. import grading
 class SubmissionsListAllHandler(NetsecHandler):
     def get(self):
         submissions = submission.get_all(self.application.db)
-        subms = [ {"submission": a_submission,
-                   "grader": ", ".join(grading.get_all_graders(self.application.db, a_submission.id)),
-                   "status": grading.get_submission_grade_status(self.application.db, a_submission.id)}
-                 for a_submission in submissions ]
-
-        self.render('submissions_list_all', {'submissions': subms})
+        self.render('submissions_list_all', {'submissions': submissions})
--- netsecus/webhandler/SubmissionsListCurrentHandler.py
+++ netsecus/webhandler/SubmissionsListCurrentHandler.py
@@ -10,4 +10,11 @@ class SubmissionsListCurrentHandler(NetsecHandler):
         # TODO only list newest submission
         # TODO include correction status
         submissions = submission.get_all(self.application.db)
-        self.render('submissions_list_current', {'submissions': submissions})
+
+        subms = [ {"submission": a_submission,
+                   "grader": ", ".join(grading.get_all_graders(self.application.db, a_submission.id)),
+                   "status": grading.get_submission_grade_status(self.application.db, a_submission.id)}
+                 for a_submission in submissions ]
+
+
+        self.render('submissions_list_current', {'submissions': subms})

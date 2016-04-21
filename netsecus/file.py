21e8cde7a2c8b3ad449848558772e21caed43cb8
maride <martin.dessauer@hhu.de>
2016-04-21 15:23:11 +0200

    Show size in files view

--- netsecus/file.py
+++ netsecus/file.py
@@ -2,14 +2,15 @@ from __future__ import unicode_literals
 
 import collections
 
-File = collections.namedtuple("File", ["id", "submission_id", "hash", "filename"])
+File = collections.namedtuple("File", ["id", "submission_id", "hash", "filename", "size"])
 
 
 def get_for_submission(db, submission_id):
-    db.cursor.execute("SELECT id, submission_id, hash, filename FROM file WHERE submission_id = ?", (submission_id, ))
+    db.cursor.execute("""SELECT id, submission_id, hash, filename, size FROM file
+                         WHERE submission_id = ?""", (submission_id, ))
     return [File(*row) for row in db.cursor.fetchall()]
 
 
 def get_from_hash(db, hash):
-    db.cursor.execute("SELECT id, submission_id, hash, filename FROM file WHERE hash = ?", (hash, ))
+    db.cursor.execute("SELECT id, submission_id, hash, filename, size FROM file WHERE hash = ?", (hash, ))
     return File(*db.cursor.fetchone())

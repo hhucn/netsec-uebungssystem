dc22955213f76f292de32f2b0e99c0c8ce21d8f7
Philipp Hagemeister <phihag@phihag.de>
2016-04-28 15:25:01 +0200

    Revert "clarify TODO"
    This reverts commit 835105cff366707fb3a7ea2c3c48586241952caa.

--- netsecus/database.py
+++ netsecus/database.py
@@ -33,7 +33,7 @@ class Database(object):
                 `files_path` text
             )""")
 
-        # TODO: Reference grading_result here
+        # Legacy table - remove
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS `grading` (
                 `id` INTEGER PRIMARY KEY AUTOINCREMENT,

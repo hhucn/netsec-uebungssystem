c3f57c10922563850e35edb14398b67b37d77da3
maride <martin.dessauer@hhu.de>
2016-05-09 13:26:28 +0200

    Fix overview page

--- netsecus/webhandler/OverviewHandler.py
+++ netsecus/webhandler/OverviewHandler.py
@@ -1,11 +1,26 @@
 from __future__ import unicode_literals
 
 from .NetsecHandler import NetsecHandler
+from .. import assignment
 from .. import grading
+from .. import student
+from .. import submission
 
 
 class OverviewHandler(NetsecHandler):
     def get(self):
         grader = self.request.netsecus_user
-        sheets = grading.get_grades_for_grader(self.application.db, grader)
-        self.render('overview', {'sheets': sheets, 'grader': grader})
+        submissions = submission.get_all_newest(self.application.db)
+        subms = []
+
+        for a_submission in submissions:
+            assigned_grader = assignment.get_for_submission(self.application.db, a_submission.id)
+            if assigned_grader.grader == grader:
+                subms.append({
+                    "submission": a_submission,
+                    "assignment": assigned_grader,
+                    "status": grading.get_submission_grade_status(self.application.db, a_submission.id),
+                    "student": student.get_full_student(self.application.db, a_submission.student_id),
+                })
+
+        self.render('submissions_list_current', {'submissions': subms})

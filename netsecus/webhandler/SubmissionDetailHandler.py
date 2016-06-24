3a881dea4c558e3b012f2b4e89717b47bee5d360
maride <martin.dessauer@hhu.de>
2016-06-24 19:28:32 +0200

    Show reached and total score in Detail page

--- netsecus/webhandler/SubmissionDetailHandler.py
+++ netsecus/webhandler/SubmissionDetailHandler.py
@@ -17,11 +17,16 @@ class SubmissionDetailHandler(NetsecHandler):
         tasks = task.get_for_sheet(self.application.db, realsubmission["sheet_id"])
         available_graders = grading.get_available_graders(self.application.config)
 
-        graded_tasks = [{
-            "task": a_task,
-            "grading": grading.get_grade_for_task(
-                self.application.db, a_task.id, realsubmission["id"]),
-        } for a_task in tasks]
+        total_score = 0
+        reached_score = 0
+        graded_tasks = []
+
+        for a_task in tasks:
+            grading_for_task = grading.get_grade_for_task(self.application.db, a_task.id, realsubmission["id"])
+            graded_tasks.append({ "task": a_task, "grading": grading_for_task })
+            total_score += a_task.decipoints
+            if grading_for_task:
+                reached_score += grading_for_task.decipoints
 
         self.render('submissionDetail', {
             'submission': realsubmission,
@@ -30,4 +35,6 @@ class SubmissionDetailHandler(NetsecHandler):
             'alias': realsubmission["primary_alias"],
             'grader': realsubmission["grader"],
             'available_graders': available_graders,
+            'reached_score': reached_score,
+            'total_score': total_score
         })

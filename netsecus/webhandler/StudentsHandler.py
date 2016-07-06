9921572f4a0c6ca0cf2b710826e7520f314fd3c1
maride <martin.dessauer@hhu.de>
2016-07-06 19:53:47 +0200

    Show reached points in students list view

--- netsecus/webhandler/StudentsHandler.py
+++ netsecus/webhandler/StudentsHandler.py
@@ -7,4 +7,12 @@ from .. import student
 class StudentsHandler(NetsecHandler):
     def get(self):
         full_students = student.get_full_students(self.application.db)
-        self.render('students', {'full_students': full_students})
+        students_with_scores = []
+
+        for fs in full_students:
+            students_with_scores.append({
+                "student": fs,
+                "score": student.get_student_total_score(self.application.db, fs.student.id)
+            })
+
+        self.render('students', {'full_students': students_with_scores})

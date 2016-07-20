124e0afa8971d76f34df2ca761ef8836475778e0
maride <martin.dessauer@hhu.de>
2016-07-20 09:28:26 +0200

    Move "Read content for hash" to "file"

--- netsecus/file.py
+++ netsecus/file.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 import collections
+import os
+
+from . import submission
 
 File = collections.namedtuple("File", ["id", "submission_id", "hash", "filename", "size"])
 
@@ -14,3 +17,14 @@ def get_for_submission(db, submission_id):
 def get_from_hash(db, hash):
     db.cursor.execute("SELECT id, submission_id, hash, filename, size FROM file WHERE hash = ?", (hash, ))
     return File(*db.cursor.fetchone())
+
+
+def get_content_for_hash(db, config, hash):
+    requested_file = get_from_hash(db, hash)
+    file_submission = submission.get_from_id(db, requested_file.submission_id)
+    requested_file_relpath = os.path.join(file_submission.files_path, requested_file.filename)
+    requested_file_abspath = os.path.join(config.module_path, requested_file_relpath)
+
+    with open(requested_file_abspath, "rb") as requested_file_handle:
+        content = requested_file_handle.read()
+        return content
--- netsecus/webhandler/DownloadHandler.py
+++ netsecus/webhandler/DownloadHandler.py
@@ -11,11 +11,7 @@ from .. import submission
 
 class DownloadHandler(RequestHandlerWithAuth):
     def get(self, hash):
-        requested_file = file.get_from_hash(self.application.db, hash)
-        file_submission = submission.get_from_id(self.application.db, requested_file.submission_id)
-        requested_file_relpath = os.path.join(file_submission.files_path, requested_file.filename)
-        requested_file_abspath = os.path.join(self.application.config.module_path, requested_file_relpath)
-
+        requested_file = file.get_from_hash(db, hash)
         requested_file_type, encoding = mimetypes.guess_type(requested_file.filename)
 
         if not requested_file_type:
@@ -24,6 +20,4 @@ class DownloadHandler(RequestHandlerWithAuth):
         self.set_header("Content-Type", requested_file_type)
         self.set_header("Content-Disposition", "attachment; filename=%s" % requested_file.filename)
 
-        with open(requested_file_abspath, "rb") as requested_file_handle:
-            content = requested_file_handle.read()
-            self.write(content)
+        self.write(file.get_content_for_hash(self.application.db, self.config, hash))

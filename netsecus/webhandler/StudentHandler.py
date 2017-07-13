b6e8ade64160cdf1e10e1c735f95324735d16f2a
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-07-13 23:44:51 +0200

    Remove whitespaces in blank line for PEP8

--- netsecus/webhandler/StudentHandler.py
+++ netsecus/webhandler/StudentHandler.py
@@ -20,7 +20,7 @@ class StudentHandler(NetsecHandler):
         submission_with_score = []
         student_total_score = 0
         added_sheets = set()
-        
+
         for subm in fs.submissions:
             student_score = 0
 
@@ -35,7 +35,7 @@ class StudentHandler(NetsecHandler):
                         "student_score": student_score,
                         "total_score": total_score,
                     })
-                    
+
                     if subm.sheet_id not in added_sheets:
                         student_total_score += student_score
                         added_sheets.add(subm.sheet_id)
